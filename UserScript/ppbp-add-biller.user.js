f86e7680d18b3939cc8e19539b7b3ca8e845a313
Apocalypsor <contact@changqixiao.com>
2024-02-15 19:43:02 -0500

    Update
    Signed-off-by: Apocalypsor <contact@changqixiao.com>

--- UserScript/ppbp-add-biller.user.js
+++ UserScript/ppbp-add-biller.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PayPal Add Biller
 // @namespace    http://tampermonkey.net/
-// @version      0.12.1
+// @version      0.12.4
 // @description  Add old billers to PayPal Bill Pay
 // @author       Apocalypsor
 // @match        https://www.paypal.com/myaccount/payments/*
@@ -12,7 +12,7 @@
 // @downloadURL  https://raw.githubusercontent.com/Apocalypsor/Rules/master/UserScript/ppbp-add-biller.user.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     const creditCardPayees = [
@@ -20,169 +20,61 @@
             externalId: "616ffde06b8cee6bdb96280a-46494e414e4349414c5345525649434553",
             ownerId: "c046047f-0197-43d6-a057-886b6f99817a",
             displayName: "American Express – Personal Cards",
-            logoUrl: {
-                href: "https://secure2.paymentus.com/rotp/www/images/rpps/American_Express.png"
-            },
-            alias: "American Express",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "american",
-                    "express"
-                ],
-                alias: []
-            }
+            logoUrl: "https://secure2.paymentus.com/rotp/www/images/rpps/American_Express.png"
         },
         {
             externalId: "308",
             ownerId: "ee5a500b-159a-4527-aefa-8d93572b39a3",
             displayName: "Bank of America",
-            logoUrl: {
-                href: "https://images.getbills.com/logos/0000000308001.gif"
-            },
-            alias: "Bank of America",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "bank",
-                    "of",
-                    "america"
-                ],
-                alias: []
-            }
+            logoUrl: "https://images.getbills.com/logos/0000000308001.gif"
         },
         {
             externalId: "578",
             ownerId: "ee5a500b-159a-4527-aefa-8d93572b39a3",
             displayName: "Citi",
-            logoUrl: {
-                href: "https://images.getbills.com/logos/0000006827001.jpg"
-            },
-            alias: "Citi",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "citi"
-                ],
-                alias: []
-            }
+            logoUrl: "https://images.getbills.com/logos/0000006827001.jpg"
         },
         {
             externalId: "5314",
             ownerId: "ee5a500b-159a-4527-aefa-8d93572b39a3",
             displayName: "Discover Card",
-            logoUrl: {
-                href: "https://images.getbills.com/logos/0000005314001_discover.jpg"
-            },
-            alias: "Discover Card",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "discover"
-                ],
-                alias: []
-            }
+            logoUrl: "https://images.getbills.com/logos/0000005314001_discover.jpg"
         },
         {
             externalId: "62455e87d82af71c627f5090",
             ownerId: "c046047f-0197-43d6-a057-886b6f99817a",
             displayName: "Wells Fargo",
-            logoUrl: {
-                href: "https://secure2.paymentus.com/rotp/www/images/rpps/Wells%20Fargo_logo.png"
-            },
-            alias: "Wells Fargo",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "wells",
-                    "fargo"
-                ],
-                alias: []
-            }
+            logoUrl: "https://secure2.paymentus.com/rotp/www/images/rpps/Wells%20Fargo_logo.png"
         },
         {
             externalId: "5f91ed869c92e7f690fd4b2d",
             ownerId: "c046047f-0197-43d6-a057-886b6f99817a",
             displayName: "US Bank",
-            logoUrl: {
-                href: "https://secure2.paymentus.com/rotp/www/images/rpps/US%20Bank_logo.png"
-            },
-            alias: "US Bank",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "us",
-                    "bank"
-                ],
-                alias: []
-            }
+            logoUrl: "https://secure2.paymentus.com/rotp/www/images/rpps/US%20Bank_logo.png"
         },
         {
             externalId: "623b776077c015daf581708c",
             ownerId: "c046047f-0197-43d6-a057-886b6f99817a",
             displayName: "Capital One",
-            logoUrl: {
-                href: "https://secure2.paymentus.com/rotp/www/images/rpps/Capital_One.png"
-            },
-            alias: "Capital One",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "capital",
-                    "one"
-                ],
-                alias: []
-            }
+            logoUrl: "https://secure2.paymentus.com/rotp/www/images/rpps/Capital_One.png"
         },
         {
             externalId: "5f91ed1f9c92e7f690faad47",
             ownerId: "c046047f-0197-43d6-a057-886b6f99817a",
             displayName: "HSBC",
-            logoUrl: {
-                href: "https://secure2.paymentus.com/rotp/www/images/rpps/HSBC.png"
-            },
-            alias: "HSBC",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "hsbc"
-                ],
-                alias: []
-            }
+            logoUrl: "https://secure2.paymentus.com/rotp/www/images/rpps/HSBC.png"
         },
         {
             externalId: "5f91ecc29c92e7f690f8608a",
             ownerId: "c046047f-0197-43d6-a057-886b6f99817a",
             displayName: "Apple Card",
-            logoUrl: {
-                href: "https://pbs.twimg.com/profile_images/1163895675513274368/gk5MAZvy_400x400.png"
-            },
-            alias: "Apple Card",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "apple",
-                    "card"
-                ],
-                alias: []
-            }
+            logoUrl: "https://pbs.twimg.com/profile_images/1163895675513274368/gk5MAZvy_400x400.png"
         },
         {
             externalId: "5f91ed799c92e7f690fcfe95",
             ownerId: "c046047f-0197-43d6-a057-886b6f99817a",
             displayName: "Target RedCard",
-            logoUrl: {
-                href: "https://secure2.paymentus.com/rotp/www/images/rpps/Target.png"
-            },
-            alias: "Target RedCard",
-            category: "CREDITCARDS",
-            highlights: {
-                displayName: [
-                    "target",
-                    "redcard"
-                ],
-                alias: []
-            }
+            logoUrl: "https://secure2.paymentus.com/rotp/www/images/rpps/Target.png"
         },
     ];
 
@@ -191,41 +83,37 @@
             externalId: "5f91ed399c92e7f690fb6050",
             ownerId: "c046047f-0197-43d6-a057-886b6f99817a",
             displayName: "Maria Pappas Cook County Treasurer",
-            logoUrl: {
-                href: "https://cdn.finovera.com/static/direct/MariaPappasCookCountyTreasurer_logo.png"
-            },
-            alias: "Maria Pappas Cook County Treasurer",
-            category: null,
-            highlights: {
-                displayName: [
-                    "treasurer"
-                ],
-                alias: []
-            }
+            logoUrl: "https://cdn.finovera.com/static/direct/MariaPappasCookCountyTreasurer_logo.png",
         },
     ];
 
+    const mapPayee = (d) => ({
+        externalId: d.externalId,
+        ownerId: d.ownerId,
+        displayName: d.displayName,
+        logoUrl: { href: d.logoUrl },
+        alias: null,
+        category: null,
+        highlights: { displayName: [], alias: [] },
+    });
+
     const originalOpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function() {
-        this.addEventListener('readystatechange', function() {
-            if (this.readyState === 4 && (this.responseURL.includes('suggested-payees/ppbp?') || this.responseURL.includes('suggested-payees/pptax?'))) {
+    XMLHttpRequest.prototype.open = function () {
+        this.addEventListener('readystatechange', function () {
+            if (this.readyState === 4 && /suggested-payees\/pp(bp|tax)\?/.test(this.responseURL)) {
                 try {
                     const json = JSON.parse(this.responseText);
+                    const isCreditCard = this.responseURL.includes('suggested-payees/ppbp?');
 
-                    if (this.responseURL.includes('suggested-payees/ppbp?')) {
-                        json.data.payees.payees = creditCardPayees;
-                        json.data.payees.totalItems = creditCardPayees.length;
-                        json.data.payees.totalPages = 1;
-                    } else if (this.responseURL.includes('suggested-payees/pptax?')) {
-                        json.data.payees.payees = taxPayees;
-                        json.data.payees.totalItems = taxPayees.length;
-                        json.data.payees.totalPages = 1;
-                    }
+                    const selectedPayees = isCreditCard ? creditCardPayees : taxPayees;
 
+                    json.data.payees.payees = selectedPayees.map(mapPayee);
+                    json.data.payees.totalItems = selectedPayees.length;
+                    json.data.payees.totalPages = 1;
 
                     const modifiedResponse = JSON.stringify(json);
-                    Object.defineProperty(this, 'response', {writable: true});
-                    Object.defineProperty(this, 'responseText', {writable: true});
+                    Object.defineProperty(this, 'response', { writable: true });
+                    Object.defineProperty(this, 'responseText', { writable: true });
                     this.response = modifiedResponse;
                     this.responseText = modifiedResponse;
                 } catch (e) {
@@ -236,4 +124,5 @@
 
         originalOpen.apply(this, arguments);
     };
+
 })();
